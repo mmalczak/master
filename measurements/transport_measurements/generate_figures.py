d5e3750109786b8f2428994187dd8b46dcd5206d
Milosz Malczak <milosz.malczak@cern.ch>
2019-07-15 20:08:32 +0200

    Updated script for generating the figures

--- measurements/transport_measurements/generate_figures.py
+++ measurements/transport_measurements/generate_figures.py
@@ -5,12 +5,24 @@ from parse import read_data
 
 plt.rcParams["font.family"] = "serif"
 
-def add_data_to_fig(data, label):
-    plt.plot(data['postsamples'], data['medium'], label=label)
+def add_data_to_fig(data, label, xlabel, ylabel):
+    #plt.plot(data['postsamples'], data['medium'], label=label)
     #plt.semilogx(data['postsamples'], data['medium'], label=label)
-    #plt.errorbar(data['postsamples'], data['medium'],
-    #             yerr=data['std_dev'], fmt='-o', label='bleble')
+    plt.subplot(211)
+    plt.errorbar((data['postsamples']), data['medium'],
+                 yerr=data['std_dev'], label=label)
     plt.legend(loc='best')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.subplot(212)
+    plt.errorbar((data['postsamples']), data['medium'],
+                 yerr=data['std_dev'], label=label)
+    plt.xscale('log')
+    plt.legend(loc='best')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.tight_layout(pad=0)
+
 
 meas_path = '/home/milosz/Projects/master/measurements/transport_measurements/'
 save_path = '/home/milosz/Projects/master/figures/measurements/'
@@ -47,42 +59,45 @@ TCP_protobuf_dat_4_chan = read_data(file_name)[3]
 
 
 
-add_data_to_fig(XML_dat_4_chan, 'XMLRPC')
-add_data_to_fig(ZMQ_pickle_dat_4_chan, 'ZMQ')
-add_data_to_fig(TCP_pickle_dat_4_chan, 'TCP')
-plt.xlabel('postsamples')
-plt.ylabel('acquisition time [s]')
+add_data_to_fig(XML_dat_4_chan, 'XMLRPC', 'postsamples',
+                'acquisition time [s]')
+add_data_to_fig(ZMQ_pickle_dat_4_chan, 'ZMQ', 'postsamples',
+                'acquisition time [s]')
+add_data_to_fig(TCP_pickle_dat_4_chan, 'TCP', 'postsamples',
+                'acquisition time [s]')
 plt.savefig(save_path + 'xml_zmq_tcp_comparison_pickle.svg', format='svg')
 
 plt.clf()
-add_data_to_fig(ZMQ_pickle_dat_4_chan, 'ZMQ')
-add_data_to_fig(TCP_pickle_dat_4_chan, 'TCP')
-plt.xlabel('postsamples')
-plt.ylabel('acquisition time [s]')
+add_data_to_fig(ZMQ_pickle_dat_4_chan, 'ZMQ', 'postsamples',
+                'acquisition time [s]') 
+add_data_to_fig(TCP_pickle_dat_4_chan, 'TCP', 'postsamples',
+                'acquisition time [s]')
 plt.savefig(save_path + 'zmq_tcp_comparison_pickle.svg', format='svg')
 
 plt.clf()
-add_data_to_fig(ZMQ_pickle_dat_4_chan, 'pickle')
-add_data_to_fig(ZMQ_json_dat_4_chan, 'json')
-add_data_to_fig(ZMQ_protobuf_dat_4_chan, 'protobuf')
-plt.xlabel('postsamples')
-plt.ylabel('acquisition time [s]')
+add_data_to_fig(ZMQ_pickle_dat_4_chan, 'pickle', 'postsamples',
+                'acquisition time [s]')
+add_data_to_fig(ZMQ_json_dat_4_chan, 'json', 'postsamples',
+                'acquisition time [s]')
+add_data_to_fig(ZMQ_protobuf_dat_4_chan, 'protobuf', 'postsamples',
+                'acquisition time [s]')
 plt.savefig(save_path + 'zmq_serialization_comp.svg', format='svg')
 
 plt.clf()
-add_data_to_fig(TCP_pickle_dat_4_chan, 'pickle')
-add_data_to_fig(TCP_json_dat_4_chan, 'json')
-add_data_to_fig(TCP_protobuf_dat_4_chan, 'protobuf')
-plt.xlabel('postsamples')
-plt.ylabel('acquisition time [s]')
+add_data_to_fig(TCP_pickle_dat_4_chan, 'pickle', 'postsamples',
+                'acquisition time [s]')
+add_data_to_fig(TCP_json_dat_4_chan, 'json', 'postsamples',
+                'acquisition time [s]')
+add_data_to_fig(TCP_protobuf_dat_4_chan, 'protobuf', 'postsamples',
+                'acquisition time [s]')
 plt.savefig(save_path + 'tcp_serialization_comp.svg', format='svg')
 
 
 plt.clf()
-add_data_to_fig(ZMQ_pickle_numpy_dat_4_chan, 'ZMQ')
-add_data_to_fig(TCP_pickle_numpy_dat_4_chan, 'TCP')
-plt.xlabel('postsamples')
-plt.ylabel('acquisition time [s]')
+add_data_to_fig(ZMQ_pickle_numpy_dat_4_chan, 'ZMQ', 'postsamples',
+                'acquisition time [s]')
+add_data_to_fig(TCP_pickle_numpy_dat_4_chan, 'TCP', 'postsamples',
+                'acquisition time [s]')
 plt.savefig(save_path + 'zmq_tcp_comparison_pickle_numpy.svg', format='svg')
 
 
